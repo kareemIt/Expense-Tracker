fe43d516590c0228c84886364798ddd09f47e460
kareemIt <kareem.itani2@gmail.com>
2021-09-09 02:55:48 -0400

    adding input tags , styling divs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [balance, setBalance] = useState(0);
+  const [history, setHistory] = useState([]);
+
   return (
     <div>
-      <h1>Expensive Tracker</h1>
-      <div className="Container">
-        <div className="Balance" />
-        <p>Your Balance</p>
-        <p>$$</p>
-        <div className="Income/Expenses">
-          <p>Income</p>
-          <p>$$</p>
-          <p>Expense</p>
-          <p>$$</p>
+      <div className="container">
+        <h1>Expensive Tracker</h1>
+        <div className="balance">
+          <p>Your Balance</p>
+          <p>${balance.toFixed(2)}</p>
         </div>
-        <div className="History" />
+        <div className="income-expense">
+          <div className="income">
+            <p>Income</p>
+            <p className="number-income">$$</p>
+          </div>
+          <div className="expense">
+            <p>Expense</p>
+            <p className="number-expense">$$</p>
+          </div>
+        </div>
+        <div className="history">
+          <p>History</p>
+        </div>
         <div className="new-transactions">
           <p>Add New Transaction</p>
-          <p>text</p>
-          <input type="text" />
+          <p>Text</p>
+          <input placeHolder="Enter text" type="text" />
           <p>Amount</p>
-          <input type="text" />
+          <input placeHolder="Enter amount" type="text" />
           <button>Add transaction</button>
         </div>
       </div>
